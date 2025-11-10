e2e7945a357476c95ad6cc318da1e6d1497742a0
David Macario <david.macariopro@gmail.com>
2025-11-10 13:01:37 +0100

    fixed accounts routes, added navbar and sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_10_105743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,15 +20,15 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
     t.integer "role", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id", null: false
-    t.index [ "account_id" ], name: "index_account_users_on_account_id"
-    t.index [ "user_id" ], name: "index_account_users_on_user_id"
+    t.index ["account_id"], name: "index_account_users_on_account_id"
+    t.index ["user_id"], name: "index_account_users_on_user_id"
   end
 
   create_table "account_users_permissions", id: false, force: :cascade do |t|
     t.bigint "account_user_id", null: false
     t.bigint "permission_id", null: false
-    t.index [ "account_user_id", "permission_id" ], name: "idx_on_account_user_id_permission_id_946e947cd0"
-    t.index [ "permission_id", "account_user_id" ], name: "idx_on_permission_id_account_user_id_fcc60f2dbd"
+    t.index ["account_user_id", "permission_id"], name: "idx_on_account_user_id_permission_id_946e947cd0"
+    t.index ["permission_id", "account_user_id"], name: "idx_on_permission_id_account_user_id_fcc60f2dbd"
   end
 
   create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -37,7 +37,45 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
     t.uuid "owner_id", null: false
     t.string "slug"
     t.datetime "updated_at", null: false
-    t.index [ "owner_id" ], name: "index_accounts_on_owner_id"
+    t.index ["owner_id"], name: "index_accounts_on_owner_id"
+  end
+
+  create_table "action_text_rich_texts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.uuid "record_id", null: false
+    t.string "record_type", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.uuid "record_id", null: false
+    t.string "record_type", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.string "content_type"
+    t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "invitations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -48,8 +86,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
     t.uuid "sender_id", null: false
     t.string "token"
     t.datetime "updated_at", null: false
-    t.index [ "account_id" ], name: "index_invitations_on_account_id"
-    t.index [ "sender_id" ], name: "index_invitations_on_sender_id"
+    t.index ["account_id"], name: "index_invitations_on_account_id"
+    t.index ["sender_id"], name: "index_invitations_on_sender_id"
   end
 
   create_table "permissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -65,7 +103,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
     t.datetime "updated_at", null: false
     t.string "user_agent"
     t.uuid "user_id", null: false
-    t.index [ "user_id" ], name: "index_sessions_on_user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -76,12 +114,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_09_162215) do
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
-    t.index [ "email_address" ], name: "index_users_on_email_address", unique: true
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   add_foreign_key "account_users", "accounts"
   add_foreign_key "account_users", "users"
   add_foreign_key "accounts", "users", column: "owner_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "invitations", "accounts"
   add_foreign_key "invitations", "users", column: "sender_id"
   add_foreign_key "sessions", "users"
