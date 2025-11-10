bf9349562b3ba2fbdc9bd667189872fc267ca821
Luke O'Hare <oharelt@gmail.com>
2025-11-10 17:10:54 +0000

    link to members page on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class DashboardController < ApplicationController
   before_action :authenticate_account_access!
+  before_action :set_account
 
   def index
+  end
+
+  private
+
+  def set_account
+    @account = Account.find_by!(slug: params[:account_slug])
   end
 end
