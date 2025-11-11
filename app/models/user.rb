874ed3443365e183dbec2e9778b316c868e2a348
David Macario <david.macariopro@gmail.com>
2025-11-12 00:30:17 +0100

    update User model to affect default_account after creating

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,21 @@ class User < ApplicationRecord
   has_many :account_users, dependent: :destroy
   has_many :accounts, through: :account_users
   has_many :invitations, dependent: :destroy
+  belongs_to :default_account, optional: true, class_name: "Account"
 
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
   validates :last_name, presence: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  after_commit :create_default_account, on: :create
+  after_create :create_default_account
 
   private
   def create_default_account
     Account.transaction do
       account = Account.create!(name: self.first_name, slug: "#{SecureRandom.hex(8)}", owner: self)
       AccountUser.create!(user: self, account: account, role: :owner)
+      self.update default_account: account
     end
-    self.default_account = self.accounts.first
   end
 end
--- db/migrate/20251111230350_add_default_account_field_to_user.rb
+++ db/migrate/20251111230350_add_default_account_field_to_user.rb
@@ -1,5 +1,5 @@
 class AddDefaultAccountFieldToUser < ActiveRecord::Migration[8.1]
   def change
-    add_reference :users, :default_account, null: false, foreign_key: { to_table: :accounts }, type: :uuid
+    add_reference :users, :default_account, null: true, foreign_key: { to_table: :accounts }, type: :uuid
   end
 end
