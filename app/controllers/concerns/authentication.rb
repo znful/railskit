edecf0369913321ed36f13bd6953035fe9750654
David Macario <david.macariopro@gmail.com>
2025-11-14 23:34:17 +0100

    add default account to session on log in

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -39,7 +39,7 @@ module Authentication
     end
 
     def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip, account: user.default_account).tap do |session|
         Current.session = session
         cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
       end
