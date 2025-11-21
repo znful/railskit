60b26a293a49ee6aae991414d34004bc3cdcb05f
David Macario <david.macariopro@gmail.com>
2025-11-21 22:12:20 +0100

    make join button functional, redirect unwanted users

--- app/controllers/join_invitations_controller.rb
+++ app/controllers/join_invitations_controller.rb
@@ -1,11 +1,20 @@
 class JoinInvitationsController < ApplicationController
   before_action :set_invitation
   before_action :redirect_members, only: %i[ create ]
+  before_action :redirect_unexpected_user, only: %i[ index create ]
 
   def index
   end
 
   def create
+    account_user = AccountUser.new(user: Current.user, account: @invitation.account)
+
+    if account_user.save
+      @invitation.destroy!
+      redirect_to account_dashboard_path(@invitation.account), notice: "Successfully joined #{@invitation.account.name}"
+    else
+      render :new, notice: "Error joining account. Please try again."
+    end
   end
 
   private
@@ -15,5 +24,9 @@ class JoinInvitationsController < ApplicationController
 
   def redirect_members
     redirect_to account_dashboard_path(@invitation.account), notice: "You are already a member of this account" if AccountUser.where(account: @invitation.account, user: Current.user).exists?
+  end
+
+  def redirect_unexpected_user
+    redirect_to account_dashboard_path(Current.user.default_account), alert: "Invalid invite link." if Current.user.email_address != @invitation.receiver
   end
 end
